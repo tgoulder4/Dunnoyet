1ea705ecbdedefb55bf8372c90f53dad7fe8db1f
tgoulder4 <118992509+tgoulder4@users.noreply.github.com>
2023-09-13 03:06:48 +0100

    latest stable

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,16 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="w-full h-full grid place-items-center">
-      <div className="h-5/6 w-3/4">
-        <h2>Title</h2>
-        <section className="bg-white h-full rounded-3xl flex flex-col items-center justify-between p-10">
-          <article>
-            <h3>What was said</h3>
-            <h1>Some title text</h1>
+    <main className="w-full h-full px-48 pt-6 pb-14">
+      <div className=" flex flex-col gap-4 h-full">
+        <h2 className="font-bold primary text-3xl">The French Revolution</h2>
+        <section className="bg-white rounded-3xl h-full w-full text-center flex flex-col items-center justify-between p-10">
+          <article className="flex flex-col">
+            <h3>'social and political upheaval' - What?</h3>
+            <h1 className="text-8xl primary font-bold text-center">
+              Some title text
+            </h1>
+            {/* 'What is .? I understand -> goes back to enquired sentence which is now amended in terms of this explanation'  */}
           </article>
           <div id="actionArea">
             <Button variant="outline">Some action</Button>
