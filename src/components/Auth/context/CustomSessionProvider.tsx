fa551e5dd43c567386822fa7948c852ac3165846
Tye <tcjgoulder@gmail.com>
2024-02-16 00:35:24 +0000

    appears to be working?

--- src/components/Auth/context/CustomSessionProvider.tsx
+++ src/components/Auth/context/CustomSessionProvider.tsx
@@ -2,7 +2,6 @@
 import { SessionProvider } from 'next-auth/react'
 import React, { ReactNode } from 'react'
 import { auth } from '@/auth'
-import { redirect } from 'next/navigation'
 export default async function CustomSessionProvider({ children }: { children: ReactNode }) {
     const session = await auth()
     // if (session == null || session == undefined) {
