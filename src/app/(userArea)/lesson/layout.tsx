b3c2bd77dfa7ccd65f5f2c3e76e6edd46a01d950
Tye <tcjgoulder@gmail.com>
2024-05-17 09:09:35 +0100

    lesson grid starting

--- src/app/(userArea)/lesson/layout.tsx
+++ src/app/(userArea)/lesson/layout.tsx
@@ -1,7 +1,8 @@
 'use client'
 import { merriweather, ruda } from '@/app/fonts'
 import MainAreaNavbar from '@/components/Navbar/MainAreaNavbar'
-import { colours, sizing } from '@/lib/constants'
+import LessonSection from '@/components/UserArea/Learn/Lesson/LessonSection'
+import { colours, sizing, uiBorder } from '@/lib/constants'
 import React from 'react'
 function LessonLayout({
     children
@@ -10,7 +11,16 @@ function LessonLayout({
 }) {
     return (
         <div className={` flex flex-col h-[100vh]`} style={{ fontFamily: ruda.style.fontFamily, fontSize: sizing.globalFontSize }}>
-            {children}
+            <MainAreaNavbar style='normal' show={{ userSide: { newQuestion: false }, leftSide: { lessonTimer: false } }} />
+            {/* the only children would be in the center grid cell */}
+            <div className="grid grid-cols-3 h-full">
+                <div className="flex flex-col" style={{ borderRight: uiBorder(0.1) }}>
+                    <LessonSection style={{ borderBottom: uiBorder(0.1) }} className='learningPath'></LessonSection>
+                    <LessonSection className='brainMap'></LessonSection>
+                </div>
+                <LessonSection style={{ borderRight: uiBorder(0.1) }} className='lessonChat'>{children}</LessonSection>
+                <LessonSection className='notesArea'></LessonSection>
+            </div>
         </div>
     )
 }
