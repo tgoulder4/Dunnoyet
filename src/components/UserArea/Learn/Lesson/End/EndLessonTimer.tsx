6b529b8b772d04a4c73ece2d76ed39c76e30b04e
Tye <tcjgoulder@gmail.com>
2024-05-28 01:25:08 +0100

    endless componetn needs expnow/prior

--- src/components/UserArea/Learn/Lesson/End/EndLessonTimer.tsx
+++ src/components/UserArea/Learn/Lesson/End/EndLessonTimer.tsx
@@ -1,19 +1,24 @@
 'use client'
 import React from 'react'
 import { useTimer } from 'react-timer-hook';
+import { toast } from 'sonner';
 function EndLessonTimer() {
-    const { seconds,
-        minutes
+    const {
+        seconds
     } = useTimer({
         expiryTimestamp:
-            new Date().setSeconds(new Date().getSeconds() + 5) as any,
-        onExpire: () => window.location.href = '/home',
+            new Date().setSeconds(new Date().getSeconds() + 10) as any,
+        onExpire: () =>
+        // window.location.href = '/home'
+        // alert("10s elapsed")
+        { }
+        ,
         autoStart: true,
 
     })
     // onExpire: () => window.location.href = '/home'
     return (
-        <div>{seconds} {minutes}</div>
+        <div>{seconds}</div>
     )
 }
 
