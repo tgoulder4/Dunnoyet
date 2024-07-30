a06fe6e6c1a25cc6e071942e1f2c5cd31bf2f17c
Tye <tcjgoulder@gmail.com>
2024-07-30 16:13:54 +0100

    laptop on landing

--- src/components/Landing/MainTitleArea.tsx
+++ src/components/Landing/MainTitleArea.tsx
@@ -5,9 +5,9 @@ import { colours, maxLandingWidth, spacing } from '../../lib/constants'
 import Laptop from './Laptop'
 import Link from 'next/link'
 import NewButton from '../ui/NewButton';
-
+import { MacbookScroll } from '@/components/ui/macbookScroll'
 function MainTitleArea() {
-    return (
+    return (<>
         <section style={{ paddingLeft: spacing.padding.normalY, paddingRight: spacing.padding.normalY, maxWidth: maxLandingWidth, rowGap: spacing.gaps.largest - 20 }} className="flex flex-col justify-between items-center sm:w-inherit w-full">
             <article
                 style={{ rowGap: spacing.gaps.groupedElement }} className={`${merriweather.className} flex-1 flex flex-col w-full`}
@@ -19,15 +19,7 @@ function MainTitleArea() {
                         className={`${ruda.className} relative text-center font-bold leading-[1.4] z-20`}
                     >
                         Now THIS is learning.
-                        <svg style={{
-                            width: "100px",
-                            height: "clamp(1.5rem, 4.5vw, 3.8rem)",
-                            top: "clamp(0.5rem, 3.7vw, 6rem)",
-                            left: "clamp(17rem, 4.5vw, 3.8rem)",
-                            position: "absolute",
-                        }} viewBox="0 0 151 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M7 8C37.2867 8 67.5672 7.63789 97.8403 7.42857C113.212 7.32228 128.647 7 144 7" stroke="#5C6DFF" strokeWidth="14" strokeLinecap="round" />
-                        </svg>
+
 
                     </h1>
                 </div>
@@ -39,8 +31,10 @@ function MainTitleArea() {
                     <NewButton className='text-2xl px-[30px] py-[15px] font-bold' buttonVariant='black' actionOrLink="/home">Learn now</NewButton>
                 </div>
             </article>
-            <Laptop />
         </section>
+        <div className="bg-primary w-[100vw] p-8">
+            <MacbookScroll />
+        </div></>
     )
 }
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,9 @@
 import type { Config } from "tailwindcss";
-
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -99,6 +103,17 @@ const config: Config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [require("tailwindcss-animate"), addVariablesForColors],
 };
+// This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({
+    ":root": newVars,
+  });
+}
 export default config;
